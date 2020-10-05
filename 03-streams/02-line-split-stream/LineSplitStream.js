f48a2be87ccc06a72b64218782919a59d605ee9b
Anton Vorzhev <metalezhig@gmail.com>
2020-10-05 07:51:40 +0300

    add solution for 03-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,63 @@
-const stream = require('stream');
-const os = require('os');
+const {Transform} = require('stream');
+const {EOL} = require('os');
 
-class LineSplitStream extends stream.Transform {
+class LineSplitStream extends Transform {
   constructor(options) {
     super(options);
+    this.accumulator = '';
   }
 
-  _transform(chunk, encoding, callback) {
+
+  getAccumulator() {
+    return this.accumulator;
   }
 
-  _flush(callback) {
+  append(line) {
+    this.accumulator += line;
+  }
+
+  update(line) {
+    this.accumulator = line;
+  }
+
+  hasEOL() {
+    return this.getAccumulator().includes(EOL);
+  }
+
+  shift() {
+    const [line, ...tail] = this.getAccumulator().split(EOL);
+    this.update(tail.join(EOL));
+
+    return line;
+  }
+
+  send() {
+    this.push(this.shift());
+  }
+
+  sendIfPossible() {
+    if (this.hasEOL()) {
+      this.send();
+    }
+  }
+
+  isAccumulatorNotEmpty() {
+    return this.getAccumulator().length > 0;
+  }
+
+  _transform(chunk, _, done) {
+    this.append(chunk.toString());
+    this.sendIfPossible();
+
+    done();
+  }
+
+  _flush(done) {
+    if (this.isAccumulatorNotEmpty()) {
+      this.send();
+    }
+
+    done();
   }
 }
 
