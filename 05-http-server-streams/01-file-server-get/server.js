a3deed5daf048a798f3596ecc2077d7f36ef7361
Anton Vorzhev <metalezhig@gmail.com>
2020-10-12 12:23:44 +0300

    add solution for 05-http-server-streams 01-file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,22 +1,67 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {pipeline} = require('stream');
+const {promisify} = require('util');
+const HttpError = require('./HttpError');
 
+const DEFAULT_ERROR_STATUS = 500;
+
+const pipelinePromise = promisify(pipeline);
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+const checkNestedPath = (pathname) => {
+  if (pathname.includes('/')) {
+    throw new HttpError(400);
+  }
+};
+
+const checkFileExistence = (filepath) => {
+  if (!fs.existsSync(filepath)) {
+    throw new HttpError(404);
+  }
+};
+
+const streamFile = async (filepath, res) => {
+  try {
+    await pipelinePromise(fs.createReadStream(filepath), res);
+  } catch (err) {
+    throw new HttpError(500);
+  }
+};
+
+const sendFile = async (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+  checkNestedPath(pathname);
+  checkFileExistence(filepath);
+  await streamFile(filepath, res);
+};
 
-  switch (req.method) {
-    case 'GET':
+const handleError = (err, res) => {
+  if (err instanceof HttpError) {
+    res.statusCode = err.status;
+    res.end(err.message);
+  } else {
+    res.statusCode = DEFAULT_ERROR_STATUS;
+    res.end();
+  }
+};
 
-      break;
+const notImplementedStub = () => {
+  throw new HttpError(501, 'Not implemented');
+};
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+server.on('request', async (req, res) => {
+  try {
+    if (req.method === 'GET') {
+      await sendFile(req, res);
+    } else {
+      notImplementedStub();
+    }
+  } catch (err) {
+    handleError(err, res);
   }
 });
 
