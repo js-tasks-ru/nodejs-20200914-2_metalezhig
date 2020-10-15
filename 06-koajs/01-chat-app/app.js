80cc95ccc429c53fc54e176dd531d40935ba8e66
Anton Vorzhev <metalezhig@gmail.com>
2020-10-15 12:03:53 +0300

    add soultion for 06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,24 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const MESSAGE_EVENT = 'app:message';
+
+const getMessage = (ctx) => {
+  return new Promise((resolve) => ctx.app.once(MESSAGE_EVENT, resolve));
+};
+
 router.get('/subscribe', async (ctx, next) => {
+  ctx.response.body = await getMessage(ctx);
+  return next();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (message) {
+    ctx.app.emit(MESSAGE_EVENT, ctx.request.body.message);
+  }
+  ctx.response.status = 200;
+  return next();
 });
 
 app.use(router.routes());
