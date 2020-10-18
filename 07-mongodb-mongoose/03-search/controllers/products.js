f6868e4e233a3dda72f59238a7ae8fd31a27614c
Anton Vorzhev <metalezhig@gmail.com>
2020-10-18 15:47:53 +0300

    add soultion for 07-mongodb-mongoose 03-search

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,12 @@
-module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+const Product = require('../models/Product');
+
+module.exports.productsByQuery = async function productsByQuery(ctx) {
+  const {query} = ctx.request.query;
+  let products = [];
+
+  if (query) {
+    products = await Product.find({$text: {$search: query}});
+  }
+
+  ctx.body = {products};
 };
