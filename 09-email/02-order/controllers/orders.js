29c58cb0fffcd30993c43cb4aa7f21b1ee405891
Anton Vorzhev <metalezhig@gmail.com>
2020-10-22 15:28:34 +0300

    add solution for 09-email 02-order

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -1,8 +1,39 @@
 const Order = require('../models/Order');
+const Product = require('../models/Product');
 const sendMail = require('../libs/sendMail');
 
-module.exports.checkout = async function checkout(ctx, next) {
+module.exports.checkout = async function checkout(ctx) {
+  const user = ctx.user;
+  if (!user) {
+    ctx.throw(401);
+    return;
+  }
+
+  const {product, phone, address} = ctx.request.body || {};
+  const foundProduct = await Product.findById(product);
+  const order = new Order({
+    user: user.id,
+    product,
+    phone,
+    address,
+  });
+  await order.save();
+  await sendMail({
+    template: 'order-confirmation',
+    to: user.email,
+    subject: 'Подтверждение заказа',
+    locals: {
+      id: product,
+      product: foundProduct,
+    },
+  });
+  ctx.status = 200;
+  ctx.body = {order: order.id};
 };
 
-module.exports.getOrdersList = async function ordersList(ctx, next) {
+module.exports.getOrdersList = async function ordersList(ctx) {
+  const user = ctx.user;
+  const orders = await Order.find({user: user.id}).populate('Product');
+  ctx.status = 200;
+  ctx.body = {orders};
 };
